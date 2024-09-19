9affaf19f3d94535e1a2926692af83af14647ac3
nimi1125 <sikaku1111@gmail.com>
2024-09-20 00:25:48 +0900

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
             @csrf
 
             <!-- Email Address -->
-            <div>
+            <div class="container">
                 <x-label for="email" :value="__('Email')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
